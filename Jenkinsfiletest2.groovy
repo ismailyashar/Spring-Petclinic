4b35d374e5b4127ceb41d227a18213a967c75faa
ismailyashar <68939431+ismailyashar@users.noreply.github.com>
2020-09-25 22:47:04 -0500

    Update Jenkinsfiletest2.groovy

--- Jenkinsfiletest2.groovy
+++ Jenkinsfiletest2.groovy
@@ -3,28 +3,38 @@ properties([
         string(defaultValue: '', description: 'Input node IP', name: 'SSHNODE', trim: true)
         ])
     ])
+
 node {
-    withCredentials([sshUserPrivateKey(credentialsId: 'Jenkins-Master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: 'Jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+    
+        stage("Install git") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
+       
+        }       
         stage("Initialize") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install epel-release -y "
         }
-        stage("Install java") { 
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install java-1.8.0-openjdk-devel -y" 
-        } 
-        stage("Install git") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
+        stage("Install java") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install java-1.8.0-openjdk-devel -y"
+            
         }
-        stage("Install git") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'rm -rf spring-petclinic && git clone https://github.com/spring-projects/spring-petclinic.git && ls'"
+        stage("Clone Repo") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE }'rm -rf spring-petclinic && git clone https://github.com/spring-projects/spring-petclinic.git && ls'"
         }
-        stage("Install git") {
+        
+        stage("Install pip3") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'cd spring-petclinic/ && ./mvnw package'"
+            
         }
-        stage("Install git") {
+        stage("Built Application") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'cd spring-petclinic &&  mv target/*.jar target/app.jar'"
+            
+         stage("Built Application") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE }  java -jar spring-petclinic/target/app.jar"
+            
         }
-        stage("Install git") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } java -jar spring-petclinic/target/app.jar"
         }
-    }   
+        
+    }
 }
+
